c069e18798ed9de853ff0fee78449b657deb74f1
Isaac Maupin <Isaac.Maupin@gmail.com>
2020-07-24 14:49:33 -0400

    Initial Commit

---
+++ Celebritweet/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Celebritweet
+//
+//  Created by Isaac Maupin on 7/24/20.
+//  Copyright © 2020 Isaac Maupin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
